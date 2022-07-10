c11120c670c06d3c251c41d5782c766c6f745dfd
RKenny23 <rkenny238@hotmail.com>
2022-07-10 18:00:16 -0400

    added input controls with js validation functions

---
+++ main.js
@@ -0,0 +1,192 @@
+const form = document.getElementById('form');
+
+const fName = document.getElementById('fname');
+const lName = document.getElementById('lname');
+
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const password = document.getElementById('password');
+const cPassword = document.getElementById('cpassword');
+
+form.addEventListener('submit', (e) => {
+  if (
+    !checkFnameValue() ||
+    !checkLnameValue() ||
+    !checkEmailValue() ||
+    !checkPhoneValue() ||
+    !checkPasswordValue() ||
+    !checkCpasswordValue()
+  ) {
+    e.preventDefault();
+  } else {
+    form.submit();
+    console.log('Form submitted!');
+
+    alert('Form submitted!');
+  }
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+// Check name inputs
+
+function checkFnameValue() {
+  const fNameValue = fName.value.trim();
+
+  if (fNameValue === '') {
+    setError(fName, 'First name is required.');
+  } else {
+    setSuccess(fName);
+    return true;
+  }
+}
+
+fName.addEventListener('blur', (e) => {
+  checkFnameValue();
+});
+
+function checkLnameValue() {
+  const lNameValue = lName.value.trim();
+
+  if (lNameValue === '') {
+    setError(lName, 'Last name is required.');
+  } else {
+    setSuccess(lName);
+    return true;
+  }
+}
+
+lName.addEventListener('blur', (e) => {
+  checkLnameValue();
+});
+
+// Validate email input
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(String(email).toLowerCase());
+};
+
+function checkEmailValue() {
+  const emailValue = email.value.trim();
+
+  if (emailValue === '') {
+    setError(email, 'Email is required.');
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, 'Entered value needs to be an email address.');
+  } else if (emailValue.length < 8) {
+    setError(email, 'Email should be at least 8 characters.');
+  } else {
+    setSuccess(email);
+    return true;
+  }
+}
+
+email.addEventListener('blur', (e) => {
+  checkEmailValue();
+});
+
+// Validate Phone input
+const isValidPhone = (phone) => {
+  const re = /[\d]{10,}/;
+  return re.test(String(phone));
+};
+
+function checkPhoneValue() {
+  const phoneValue = phone.value.trim();
+
+  if (phoneValue === '') {
+    setError(phone, 'Phone is required.');
+  } else if (!isValidPhone(phoneValue)) {
+    setError(phone, 'Entered value needs to be a phone number.');
+  } else {
+    setSuccess(phone);
+    return true;
+  }
+}
+
+phone.addEventListener('blur', (e) => {
+  checkPhoneValue();
+});
+
+// phone.addEventListener('input', (e) => {
+//   checkPhoneValue();
+// });
+
+// Validate password input
+// Make sure password contains at least one digit, one lower case, one upper case, at least 8 characters.
+const isValidPassword = (password) => {
+  const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+  return re.test(String(password));
+};
+
+function checkPasswordValue() {
+  const passwordValue = password.value.trim();
+
+  if (passwordValue === '') {
+    setError(password, 'Password is required.');
+  } else if (!isValidPassword(passwordValue)) {
+    setError(
+      password,
+      'Password must contain at least one digit, one uppercase and lowercase letter, and be at least 8 characters in length.'
+    );
+  } else {
+    setSuccess(password);
+    return true;
+  }
+}
+
+password.addEventListener('blur', (e) => {
+  checkPasswordValue();
+});
+
+// Validate password confirmation input
+function checkCpasswordValue() {
+  const cPasswordValue = cPassword.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (cPasswordValue === '') {
+    setError(cPassword, 'Confirmation required.');
+  } else if (cPasswordValue !== passwordValue) {
+    setError(cPassword, "Passwords don't match.");
+  } else if (cPasswordValue === passwordValue) {
+    setSuccess(cPassword);
+    return true;
+  }
+}
+
+cPassword.addEventListener('blur', (e) => {
+  checkCpasswordValue();
+});
+
+// inputs.forEach((input) => {
+//   input.addEventListener('blur', (e) => {
+//     if (
+//       !checkNameValues() ||
+//       !checkEmailValue() ||
+//       !checkPhoneValue() ||
+//       !checkPasswordValue() ||
+//       !checkCpasswordValue()
+//     ) {
+//       setSuccess(e.target);
+//     } else {
+//       setError(e.target, 'First name is required.');
+//     }
+//   });
+// });
