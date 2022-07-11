91c0e9ca6347527f23d740c3382af4f7efecd69a
RKenny23 <rkenny238@hotmail.com>
2022-07-11 00:13:30 -0400

    #

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
   } else {
     thanks.style = 'display: flex';
-    setTimeout(form.submit(), 10000);
+    setTimeout(form.submit(), 5000);
   }
 });
 
